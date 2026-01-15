7ebb505462977e2d1f65929f5540cdf5b501a3fe
yelena <yarakelian3@gmail.com>
2026-01-15 15:16:40 -0500

    Finished Q2

---
+++ task 1/p5-rev-template/p5-rev-template/js/q2.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function setup() {
+    createCanvas(500,500);
+    fill(150, 0, 255);  
+    drawEllipse();
+}
+
+function draw() {
+       background(200); 
+
+       drawEllipse(50, 50, 80, 80, 100, 255, 255);
+       drawEllipse(150, 150, 100, 100, 255, 100, 255);
+       drawEllipse(250, 250, 40, 40, 255, 255, 100);
+      
+}
+function drawEllipse(x,y,w,h,r,g,b) {
+    noStroke();
+    fill(r, g, b);
+    ellipse(x, y, w, h);
+}
