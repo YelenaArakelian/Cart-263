175e7bf1a9c5ed5088af1db56440ecdf6abc10dd
yelena <yarakelian3@gmail.com>
2026-01-21 15:54:10 -0500

    Corrected Q6 and Finalized

--- task 1/p5-rev-template/p5-rev-template/js/q6.js
+++ task 1/p5-rev-template/p5-rev-template/js/q6.js
@@ -3,3 +3,44 @@
  */
 
 "use strict";
+
+const word = "test";
+const text_size = 28;
+const text_color = 255;
+const offset_spacing = 35; 
+
+function setup() {
+  createCanvas(600, 600);
+  background(0);
+
+  fill(text_color);
+  textSize(text_size);
+  
+  textAlign(CENTER, CENTER);
+  text(word, width / 2, height / 2);
+
+  const startX = 50;
+  const startY = 30;
+
+  // change text alignment for loops
+  textAlign(LEFT, TOP);
+
+  // Loop 1 Horizontal (0 to 9)
+  for (let i = 0; i <= 9; i++) {
+    // Use 'i' to offset the x position
+    let x = startX + (i * offset_spacing);
+    let y = startY; 
+    
+    text(i, x, y);
+  }
+
+  // Loop2 Vertical (1 to 15) 
+  for (let i = 15; i >= 1; i--) {
+    let x = startX;
+    
+    // Offset the y position
+    let y = startY + (i * offset_spacing); 
+    
+    text(i, x, y);
+  }
+}
--- task 1/p5-rev-template/p5-rev-template/js/q7.js
+++ task 1/p5-rev-template/p5-rev-template/js/q7.js
@@ -1,38 +1,5 @@
+/**
+ * TASK 1: Yéléna and Scarlett
+ */
+
 "use strict";
-
-// TEXT SETTINGS
-const word = "test";
-const textSizeValue = 28;
-const textColor = 255;
-const spacing = 20;
-
-function setup() {
-  createCanvas(400, 400);
-  background(0);
-
-  fill(textColor);
-  textSize(textSizeValue);
-  textAlign(CENTER, CENTER);
-
-  // CENTER TEXT
-  let centerX = width / 2;
-  let centerY = height / 2;
-
-  text(word, centerX, centerY);
-
-  // LOOP 1: 0 to 9
-  for (let i = 0; i < 10; i++) {
-    let x = i * spacing;
-    let y = centerY + 50;
-
-    text(i, x, y);
-  }
-
-  // LOOP 2: 15 down to 1
-  for (let i = 15; i > 0; i--) {
-    let x = centerX + 100;
-    let y = i * spacing;
-
-    text(i, x, y);
-  }
-}
